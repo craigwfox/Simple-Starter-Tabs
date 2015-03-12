543f1304f0be36d9aa120e1d9e95a4035d65eaf3
Craig Fox <craig@therichlandgroup.com>
2015-03-12 11:30:40 -0500

    cleaned up js and css. Also fleshed out admin instructions page

--- src/js/sstt.js
+++ src/js/sstt.js
@@ -1,23 +1,36 @@
 jQuery(document).ready(function($) {
   var $ssttNav = $('.sstt__nav');
   var $ssttWrapper = $('.sstt__content-wrapper');
+  var speed = 200;
 
 
   $ssttWrapper.find('.sstt__content:first-child').addClass('sstt__active');
+  $ssttNav.find('.sstt__nav__title:first-child').addClass('sstt__active');
+
+
+  var func_ssttNav = function(el, tabId) {
+    $ssttNav.find('li').removeClass('sstt__active');
+    el.addClass('sstt__active');
+  };
+
+  var func_ssttContent = function(el, tabId) {
+    $ssttWrapper.find('.sstt__content')
+      .removeClass('sstt__active')
+      .fadeOut(speed);
+    
+    setTimeout(function() {
+      $ssttWrapper.find('.sstt__content[data-tab_id="'+tabId+'"]')
+        .addClass('sstt__active')
+        .fadeIn(speed-100);
+    }, speed);
+  };
 
   $ssttNav.on('click', '.sstt__nav__title', function(event) {
     event.preventDefault();
     var $el = $(this);
     var tabId = $el.attr('data-tab_id');
 
-    $ssttNav.find('li').removeClass('sstt__active');
-    $el.addClass('sstt__active');
-
-
-    $ssttWrapper.find('.sstt__content').removeClass('sstt__active');
-    $ssttWrapper.find('.sstt__content[data-tab_id="'+tabId+'"]').addClass('sstt__active');
-    
-    console.log($el.attr('data-tab_id'));
+    func_ssttNav($el, tabId);
+    func_ssttContent($el, tabId);
   });
-  console.log('tabs loaded');
 });
--- sstt_admin.php
+++ sstt_admin.php
@@ -3,29 +3,55 @@
   <div class="sstt-admin">
     <h1 class="sstt-admin__title">Instructions</h1>
 
-    <div class="sstt-admin__instructions">
+    <div class="sstt-admin__instruction__wrapper">
       <h2 class="sstt-admin__subtitle">Shortcodes</h2>
-      <h3>Wraps each accordion pair</h3>
-      <p>[accordion_section] [/accordion_section]</p>
-      
-      <h3>Wraps the Accordion Title</h3>
-      <p>[accordion_title] [/accordion_title]</p>
 
-      <h3>Wraps the content of the accordion</h3>
-      <p>[accordion_content] [/accordion_content]</p>
+      <div class="sstt-admin__instruction">
+        <h3>Wraps for all tabbed content nav items.</h3>
+        <p>[sstt_nav] [/sstt_nav]</p>
+      </div>
+      
+      <div class="sstt-admin__instruction">
+        <h3>Individual Tab Nav Item</h3>
+        <p>tab_text is the text you see in the tab nav. tab_id is the id that will tie the nav item to its content.</p>
+        <p>[sstt_nav_title tab_text="TAB_NAV_TEXT" tab_id="TAB_ID"]</p>
+      </div>
+
+      <div class="sstt-admin__instruction">
+        <h3>Wrapper for all sstt__content containers</h3>
+        <p>[sstt_content_wrapper] [/sstt_content_wrapper]</p>
+      </div>
+      
+      <div class="sstt-admin__instruction">
+        <h3>Individual Tab Content Container</h3>
+        <p>tab_id tab_id is the id that will tie the content to its nav item</p>
+        <p>[sstt_content tab_id="TAB_ID"] [/sstt_content]</p>
+      </div>
     </div>
 
     <div class="sstt-admin__example">
       <h2 class="sstt-admin__subtitle">Usage Example</h2>
       <pre class="sstt-admin__code-block">
-        <span class="sstt-admin__scode">[accordion_section]</span>
+        <span class="sstt-admin__scode">[sstt_nav]</span>
           <span class="sstt-admin__scode">[accordion_title]</span>
-            &lt;h2&gt; Your Title Here &lt;/h2&gt;
-          <span class="sstt-admin__scode">[/accordion_title]</span>
-          <span class="sstt-admin__scode">[accordion_content]</span>
-            &lt;p&gt;Lorem ipsum dolor sit amet, consectetur adipiscing elit.&lt;/p&gt;
-          <span class="sstt-admin__scode">[/accordion_content]</span>
-        <span class="sstt-admin__scode">[/accordion_section]</span>
+          <span class="sstt-admin__scode">[sstt_nav_title tab_text="Tab 1" tab_id="tab1"]</span>
+          <span class="sstt-admin__scode">[sstt_nav_title tab_text="Tab 2" tab_id="tab2"]</span>
+          <span class="sstt-admin__scode">[sstt_nav_title tab_text="Tab 3" tab_id="tab3"]</span>
+        <span class="sstt-admin__scode">[/sstt_nav]</span>
+        <span class="sstt-admin__scode">[sstt_content_wrapper]</span>
+          <span class="sstt-admin__scode">[sstt_content tab_id="tab1"]</span>
+            &lt;h2&gt;Tab 1 Title&lt;/h2&gt;
+            &lt;p&gt;Lorem ipsum dolor sit amet.&lt;/p&gt;
+          <span class="sstt-admin__scode">[/sstt_content]</span>
+          <span class="sstt-admin__scode">[sstt_content tab_id="tab2"]</span>
+            &lt;h2&gt;Tab 2 Title&lt;/h2&gt;
+            &lt;p&gt;Lorem ipsum dolor sit amet.&lt;/p&gt;
+          <span class="sstt-admin__scode">[/sstt_content]</span>
+          <span class="sstt-admin__scode">[sstt_content tab_id="tab3"]</span>
+            &lt;h2&gt;Tab 3 Title&lt;/h2&gt;
+            &lt;p&gt;Lorem ipsum dolor sit amet.&lt;/p&gt;
+          <span class="sstt-admin__scode">[/sstt_content]</span>
+        <span class="sstt-admin__scode">[/sstt_content_wrapper]</span>
       </pre>
     </div>
   </div>
